5663ed429ed1eeac4303dee3db21dd655549cd82
igor moga <moga.igor@iisgalvanimi.edu.it>
2025-04-02 12:40:51 +0000

    s6

--- progetto/app.py
+++ progetto/app.py
@@ -1,8 +1,12 @@
-from flask import Flask #importiamo la classe flask
+from flask import Flask,render_template #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
+
+@app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
+def home():
+    return render_template("index.html")
+
+
+
 #avvio flask
 if __name__ == '__main__':
     app.run(debug=True) #aggiornamenti in tempo reale
-@app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
-def home():
-return "per ora funziona tutto"
