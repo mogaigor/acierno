c8c095fe4737dba855da973457338e62da0768b9
igor moga <moga.igor@iisgalvanimi.edu.it>
2025-04-04 08:56:15 +0000

    s7

--- progetto/app.py
+++ progetto/app.py
@@ -1,11 +1,10 @@
 from flask import Flask,render_template #importiamo la classe flask
 app = Flask(__name__) #inizializza app flask
-
+x = "ciao"
+lista = ["arancia","limone","fragola","lampone","kiwi"]
 @app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
 def home():
-    return render_template("index.html")
-
-
+    return render_template("index.html", paperino = x,lista = lista)
 
 #avvio flask
 if __name__ == '__main__':
